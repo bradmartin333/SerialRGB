bdc8263c67d176315fa7c0bde768cb673f29d060
BradDesktop <brad.martin@xdisplay.com>
2021-10-10 10:43:34 -0400

    update version and further trim .ino

--- Arduino/SerialRGB/SerialRGB.ino
+++ Arduino/SerialRGB/SerialRGB.ino
@@ -2,30 +2,26 @@ int PIN[4] = {3, 5, 6, 4}; // R G B GND
 
 void setup() {
   Serial.begin(115200);
-  while (!Serial) {
+  while (!Serial) 
+  {
     ; // Wait for serial port to connect. Needed for native USB port only
   }
 
   // Send a byte to establish contact until receiver responds
-  while (Serial.available() <= 0) {
+  while (Serial.available() <= 0) 
+  {
     Serial.println("CONNECTING...");
     delay(300);
   }
   Serial.println("LED CONTROLLER CONNECTED");
 
   for (int i = 0; i < 4; i++)
-  {
     pinMode(PIN[i], OUTPUT);
-  }
 }
 
 void loop() {
   if (Serial.available() == 3)
-  {
     for (int i = 0; i < 3; i++)
-    {
       analogWrite(PIN[i], Serial.read());
-    }
-  }
   delay(10);
 }
